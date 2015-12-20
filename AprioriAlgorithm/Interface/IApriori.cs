ae4d80c086ae5c37820e88d9cdfeb0dbfef992ab
kenji-vn <thphuong299@gmail.com>
2015-12-20 23:48:20 +0700

    First commit

---
+++ AprioriAlgorithm/Interface/IApriori.cs
@@ -0,0 +1,13 @@
+﻿using AprioriAlgorithm.DataProvider;
+
+namespace AprioriAlgorithm
+{
+    using System.Collections.Generic;
+
+    public interface IApriori
+    {
+        Output ProcessTransaction(double minSupport, double minConfidence, IEnumerable<string> items, string[] transactions);
+
+        Output ProcessTransaction2(double minSupport, double minConfidence, IEnumerable<string> items, DrillData transactions);
+    }
+}
